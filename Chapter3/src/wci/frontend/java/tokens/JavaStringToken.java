b7c18b8c7a8d0865a1d7126beca17f57abeff6d6
Brian Tan <briantan37@gmail.com>
2018-09-07 15:49:20 -0700

    Modified String Token to handled escape characters and double quotes

--- Chapter3/src/wci/frontend/java/tokens/JavaStringToken.java
+++ Chapter3/src/wci/frontend/java/tokens/JavaStringToken.java
@@ -14,6 +14,7 @@ import static wci.frontend.java.JavaErrorCode.*;
  *
  * <p>Copyright (c) 2009 by Ronald Mak</p>
  * <p>For instructional purposes only.  No warranties.</p>
+ * <p>Modified by Brian Tan</p>
  */
 public class JavaStringToken extends JavaToken
 {
@@ -39,7 +40,7 @@ public class JavaStringToken extends JavaToken
         StringBuilder valueBuffer = new StringBuilder();
 
         char currentChar = nextChar();  // consume initial quote
-        textBuffer.append('\'');
+        textBuffer.append('\"');
 
         // Get string characters.
         do {
@@ -48,26 +49,31 @@ public class JavaStringToken extends JavaToken
                 currentChar = ' ';
             }
 
-            if ((currentChar != '\'') && (currentChar != EOF)) {
+            if ((currentChar != '\"') && (currentChar != EOF)) {
                 textBuffer.append(currentChar);
                 valueBuffer.append(currentChar);
                 currentChar = nextChar();  // consume character
             }
 
-            // Quote?  Each pair of adjacent quotes represents a single-quote.
-            if (currentChar == '\'') {
-                while ((currentChar == '\'') && (peekChar() == '\'')) {
-                    textBuffer.append("''");
-                    valueBuffer.append(currentChar); // append single-quote
-                    currentChar = nextChar();        // consume pair of quotes
-                    currentChar = nextChar();
+            //Checks if there is a backslash at the current character.
+            if (currentChar == '\\') {
+            	//loops if the first two characters in current position is an escape character
+                while ((currentChar == '\\') && (checkEscapeChar(peekChar()))) {
+                	//Concatenate both characters two create an escape character string
+                	String fromChar = new String(new char[] {'\\', peekChar()});;
+                	//add the string into the buffer
+                    textBuffer.append(fromChar);
+                    //Call the insertEscape method to create the literal escape character
+                    valueBuffer.append(insertEscape(peekChar())); // append
+                    currentChar = nextChar();        // consume the backslash
+                    currentChar = nextChar();		 // consume the escape character
                 }
             }
-        } while ((currentChar != '\'') && (currentChar != EOF));
+        } while ((currentChar != '\"') && (currentChar != EOF));	//keep looping till the end of quote or file
 
-        if (currentChar == '\'') {
+        if (currentChar == '\"') {
             nextChar();  // consume final quote
-            textBuffer.append('\'');
+            textBuffer.append('\"');
 
             type = STRING;
             value = valueBuffer.toString();
@@ -78,5 +84,28 @@ public class JavaStringToken extends JavaToken
         }
 
         text = textBuffer.toString();
+    }
+    
+    protected boolean checkEscapeChar(char input) {
+    	//checks the character after the backslash if it is escape character
+    	if (input == '\"' || input == 't' || input == 'n' || input == '\'' || input == '\\') { 
+    		return true;
+    	}
+    	return false;
+    }
+    
+    protected char insertEscape(char input) {
+    	char value;
+    	//gets the 2nd character and returns the literal escape character; if not found, then returns @ 
+    	switch(input) {
+    	case '\"' : value = '\"';
+    	case 't'  : value = '\t';
+    	case 'n'  : value = '\n';
+    	case '\'' : value = '\'';
+    	case '\\' : value = '\\';
+    	default   : value = '@';
+    	}
+    	return value;	
+    	
     }
 }
