0d17d42ba6e00924c5663673a695a2d00a010a5c
Andruxa0125 <andruxa0125@gmail.com>
2018-09-09 13:23:20 -0700

    Created a set of escaped characters in JavaToken.
    Set of escaped characters used for JavaWordToken and JavaCharToken.

--- Chapter3/src/wci/frontend/java/JavaToken.java
+++ Chapter3/src/wci/frontend/java/JavaToken.java
@@ -1,5 +1,7 @@
 package wci.frontend.java;
 
+import java.util.HashSet;
+
 import wci.frontend.*;
 
 /**
@@ -17,6 +19,23 @@ public class JavaToken extends Token
      * @param source the source from where to fetch the token's characters.
      * @throws Exception if an error occurred.
      */
+	
+	/* Method that creates a set with all characters that should be escaped. */
+	public static HashSet<Character> createEscapeCharsHashSet() {
+	    HashSet<Character> set = new HashSet<Character>();
+	    set.add('\''); //single quote
+	    set.add('\\'); // / backslash
+	    set.add('\"'); // double quote
+	    // \t \b \n \r \f
+	    String escapeChars = "tbnrf";
+	    for (int i = 0; i < escapeChars.length(); i++) {
+	        set.add(escapeChars.charAt(i));
+	    }
+	    return set;
+	}
+	/* Actual set used in JavaCharToken and JavaStringToken. */
+	protected static final HashSet<Character> escapeCharacters = 
+											  createEscapeCharsHashSet();
     protected JavaToken(Source source)
         throws Exception
     {
