7c22fa5cc4ad10b7a2d957e992288c283c5246f4
Izzy Moriguchi <izumimoriguchi@gmail.com>
2018-11-27 23:38:41 -0800

    Added logic to have filename as the name of jasmin file

--- Assignment6/Fantastic/src/Pcl2.java
+++ Assignment6/Fantastic/src/Pcl2.java
@@ -4,6 +4,7 @@ import org.antlr.v4.runtime.tree.ParseTree;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class Pcl2 
 {
@@ -17,15 +18,22 @@ public class Pcl2
                                 : System.in;
         
         CharStream cs = CharStreams.fromStream(is);
-        Pcl2Lexer lexer = new Pcl2Lexer(cs);
+        FantasticLexer lexer = new FantasticLexer(cs);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
-        Pcl2Parser parser = new Pcl2Parser(tokens);
-        ParseTree tree = parser.program();
-        
-        Pass1Visitor pass1 = new Pass1Visitor();
+        FantasticParser parser = new FantasticParser(tokens);
+        ParseTree tree = parser.prog();
+
+        if (inputFile == null) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter the input file name: ");
+            inputFile = scanner.next();
+        }
+
+        Pass1Visitor pass1 = new Pass1Visitor(inputFile.split("[.]")[0]);
         pass1.visit(tree);
-        
+
         PrintWriter jFile = pass1.getAssemblyFile();
+        jFile.close();
 
         Pass2Visitor pass2 = new Pass2Visitor(jFile);
         pass2.visit(tree);
