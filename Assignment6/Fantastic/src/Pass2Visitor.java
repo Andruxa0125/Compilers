71b427fbf2e87b4b9174ee97e3b65103ac9e742b
Izzy Moriguchi <izumimoriguchi@gmail.com>
2018-11-27 23:35:37 -0800

    Added TODOs

--- Assignment6/Fantastic/src/Pass2Visitor.java
+++ Assignment6/Fantastic/src/Pass2Visitor.java
@@ -12,194 +12,171 @@ public class Pass2Visitor extends FantasticBaseVisitor<Integer>
     {
         this.jFile = jFile;
     }
-    
-    @Override 
-    public Integer visitProgram(Pcl2Parser.ProgramContext ctx) 
-    { 
-        Integer value = visitChildren(ctx); 
-        jFile.close();
-        return value;
-    }
-    
-    @Override 
-    public Integer visitHeader(Pcl2Parser.HeaderContext ctx) 
-    { 
-        programName = ctx.IDENTIFIER().toString();       
-        return visitChildren(ctx); 
-    }
-    
-    @Override 
-    public Integer visitMainBlock(Pcl2Parser.MainBlockContext ctx) 
-    { 
-        // Emit the main program header.
-        jFile.println();
-        jFile.println(".method public static main([Ljava/lang/String;)V");
-        jFile.println();
-        jFile.println("\tnew RunTimer");
-        jFile.println("\tdup");
-        jFile.println("\tinvokenonvirtual RunTimer/<init>()V");
-        jFile.println("\tputstatic        " + programName + "/_runTimer LRunTimer;");
-        jFile.println("\tnew PascalTextIn");
-        jFile.println("\tdup");
-        jFile.println("\tinvokenonvirtual PascalTextIn/<init>()V");
-        jFile.println("\tputstatic        " + programName + "/_standardIn LPascalTextIn;");
-        
-        Integer value = visitChildren(ctx);
-        
-        // Emit the main program epilogue.
-        jFile.println();
-        jFile.println("\tgetstatic     " + programName + "/_runTimer LRunTimer;");
-        jFile.println("\tinvokevirtual RunTimer.printElapsedTime()V");
-        jFile.println();
-        jFile.println("\treturn");
-        jFile.println();
-        jFile.println(".limit locals 16");
-        jFile.println(".limit stack 16");
-        jFile.println(".end method");
-        
-        return value;
+
+    // TODO: Override some of the following methods (not all)
+
+    @Override
+    public Integer visitProg(FantasticParser.ProgContext ctx) {
+        return super.visitProg(ctx);
     }
 
-    @Override 
-    public Integer visitStmt(Pcl2Parser.StmtContext ctx) 
-    { 
-        jFile.println("\n; " + ctx.getText() + "\n");
-        
-        return visitChildren(ctx); 
-    }
-    @Override 
-    public Integer visitAssignmentStmt(Pcl2Parser.AssignmentStmtContext ctx)
-    {
-        Integer value = visit(ctx.expr());
-        
-        String typeIndicator = (ctx.expr().type == Predefined.integerType) ? "I"
-                             : (ctx.expr().type == Predefined.realType)    ? "F"
-                             :                                    "?";
-        
-        // Emit a field put instruction.
-        jFile.println("\tputstatic\t" + programName
-                           +  "/" + ctx.variable().IDENTIFIER().toString() 
-                           + " " + typeIndicator);
-
-        return value; 
+    @Override
+    public Integer visitIfStat(FantasticParser.IfStatContext ctx) {
+        return super.visitIfStat(ctx);
     }
 
-    @Override 
-    public Integer visitAddSubExpr(Pcl2Parser.AddSubExprContext ctx)
-    {
-        Integer value = visitChildren(ctx);
-                        
-        TypeSpec type1 = ctx.expr(0).type;
-        TypeSpec type2 = ctx.expr(1).type;
-        
-        boolean integerMode =    (type1 == Predefined.integerType)
-                              && (type2 == Predefined.integerType);
-        boolean realMode    =    (type1 == Predefined.realType)
-                              && (type2 == Predefined.realType);
-        
-        String op = ctx.addSubOp().getText();
-        String opcode;
-
-        if (op.equals("+")) {
-            opcode = integerMode ? "iadd"
-                   : realMode    ? "fadd"
-                   :               "????";
-        }
-        else {
-            opcode = integerMode ? "isub"
-                   : realMode    ? "fsub"
-                   :               "????";
-        }
-        
-        // Emit an add or subtract instruction.
-        jFile.println("\t" + opcode);
-        
-        return value; 
+    @Override
+    public Integer visitPrintStat(FantasticParser.PrintStatContext ctx) {
+        return super.visitPrintStat(ctx);
     }
 
-    @Override 
-    public Integer visitMulDivExpr(Pcl2Parser.MulDivExprContext ctx)
-    {
-        Integer value = visitChildren(ctx);
-                
-        TypeSpec type1 = ctx.expr(0).type;
-        TypeSpec type2 = ctx.expr(1).type;
-        
-        boolean integerMode =    (type1 == Predefined.integerType)
-                              && (type2 == Predefined.integerType);
-        boolean realMode    =    (type1 == Predefined.realType)
-                              && (type2 == Predefined.realType);
-        
-        String op = ctx.mulDivOp().getText();
-        String opcode;
-
-        if (op.equals("*")) {
-            opcode = integerMode ? "imul"
-                   : realMode    ? "fmul"
-                   :               "f???";
-        }
-        else {
-            opcode = integerMode ? "idiv"
-                   : realMode    ? "fdiv"
-                   :               "????";
-        }
-        
-        // Emit a multiply or divide instruction.
-        jFile.println("\t" + opcode);
-        
-        return value; 
+    @Override
+    public Integer visitVarDeclStat(FantasticParser.VarDeclStatContext ctx) {
+        return super.visitVarDeclStat(ctx);
     }
 
-    @Override 
-    public Integer visitVariableExpr(Pcl2Parser.VariableExprContext ctx)
-    {
-        String variableName = ctx.variable().IDENTIFIER().toString();
-        TypeSpec type = ctx.type;
-        
-        String typeIndicator = (type == Predefined.integerType) ? "I"
-                             : (type == Predefined.realType)    ? "F"
-                             :                                    "?";
-        
-        // Emit a field get instruction.
-        jFile.println("\tgetstatic\t" + programName +
-                      "/" + variableName + " " + typeIndicator);
-        
-        return visitChildren(ctx); 
-    }
-    
-    @Override 
-    public Integer visitSignedNumber(Pcl2Parser.SignedNumberContext ctx)
-    {
-        Integer value = visitChildren(ctx);         
-        TypeSpec type = ctx.number().type;
-        
-        if (ctx.sign().getChild(0) == ctx.sign().SUB_OP()) {
-            String opcode = (type == Predefined.integerType) ? "ineg"
-                          : (type == Predefined.realType)    ? "fneg"
-                          :                                    "?neg";
-            
-            // Emit a negate instruction.
-            jFile.println("\t" + opcode);
-        }
-        
-        return value;
+    @Override
+    public Integer visitFuncDeclStat(FantasticParser.FuncDeclStatContext ctx) {
+        return super.visitFuncDeclStat(ctx);
     }
 
-    @Override 
-    public Integer visitIntegerConst(Pcl2Parser.IntegerConstContext ctx)
-    {
-        // Emit a load constant instruction.
-        jFile.println("\tldc\t" + ctx.getText());
-        
-        return visitChildren(ctx); 
+    @Override
+    public Integer visitAssignStat(FantasticParser.AssignStatContext ctx) {
+        return super.visitAssignStat(ctx);
     }
 
-    @Override 
-    public Integer visitFloatConst(Pcl2Parser.FloatConstContext ctx)
-    {
-        // Emit a load constant instruction.
-        jFile.println("\tldc\t" + ctx.getText());
-        
-        return visitChildren(ctx); 
+    @Override
+    public Integer visitReturnStat(FantasticParser.ReturnStatContext ctx) {
+        return super.visitReturnStat(ctx);
+    }
+
+    @Override
+    public Integer visitWhileStat(FantasticParser.WhileStatContext ctx) {
+        return super.visitWhileStat(ctx);
+    }
+
+    @Override
+    public Integer visitNewLineStat(FantasticParser.NewLineStatContext ctx) {
+        return super.visitNewLineStat(ctx);
+    }
+
+    @Override
+    public Integer visitVar_decl_statement(FantasticParser.Var_decl_statementContext ctx) {
+        return super.visitVar_decl_statement(ctx);
+    }
+
+    @Override
+    public Integer visitFunc_decl_statement(FantasticParser.Func_decl_statementContext ctx) {
+        return super.visitFunc_decl_statement(ctx);
+    }
+
+    @Override
+    public Integer visitDeclarationOver(FantasticParser.DeclarationOverContext ctx) {
+        return super.visitDeclarationOver(ctx);
+    }
+
+    @Override
+    public Integer visitAssignmentOver(FantasticParser.AssignmentOverContext ctx) {
+        return super.visitAssignmentOver(ctx);
+    }
+
+    @Override
+    public Integer visitIf_statement(FantasticParser.If_statementContext ctx) {
+        return super.visitIf_statement(ctx);
+    }
+
+    @Override
+    public Integer visitReturn_statement(FantasticParser.Return_statementContext ctx) {
+        return super.visitReturn_statement(ctx);
+    }
+
+    @Override
+    public Integer visitType(FantasticParser.TypeContext ctx) {
+        return super.visitType(ctx);
+    }
+
+    @Override
+    public Integer visitReturn_type(FantasticParser.Return_typeContext ctx) {
+        return super.visitReturn_type(ctx);
+    }
+
+    @Override
+    public Integer visitFunction_name(FantasticParser.Function_nameContext ctx) {
+        return super.visitFunction_name(ctx);
+    }
+
+    @Override
+    public Integer visitRef_type(FantasticParser.Ref_typeContext ctx) {
+        return super.visitRef_type(ctx);
+    }
+
+    @Override
+    public Integer visitCompOpeOver(FantasticParser.CompOpeOverContext ctx) {
+        return super.visitCompOpeOver(ctx);
+    }
+
+    @Override
+    public Integer visitFuncCall(FantasticParser.FuncCallContext ctx) {
+        return super.visitFuncCall(ctx);
+    }
+
+    @Override
+    public Integer visitLit(FantasticParser.LitContext ctx) {
+        return super.visitLit(ctx);
+    }
+
+    @Override
+    public Integer visitVar(FantasticParser.VarContext ctx) {
+        return super.visitVar(ctx);
+    }
+
+    @Override
+    public Integer visitParens(FantasticParser.ParensContext ctx) {
+        return super.visitParens(ctx);
+    }
+
+    @Override
+    public Integer visitAddSubOver(FantasticParser.AddSubOverContext ctx) {
+        return super.visitAddSubOver(ctx);
+    }
+
+    @Override
+    public Integer visitMulDivPercOver(FantasticParser.MulDivPercOverContext ctx) {
+        return super.visitMulDivPercOver(ctx);
+    }
+
+    @Override
+    public Integer visitVariable(FantasticParser.VariableContext ctx) {
+        return super.visitVariable(ctx);
+    }
+
+    @Override
+    public Integer visitIntLitOver(FantasticParser.IntLitOverContext ctx) {
+        return super.visitIntLitOver(ctx);
+    }
+
+    @Override
+    public Integer visitStrLitOver(FantasticParser.StrLitOverContext ctx) {
+        return super.visitStrLitOver(ctx);
+    }
+
+    @Override
+    public Integer visitFunc_call(FantasticParser.Func_callContext ctx) {
+        return super.visitFunc_call(ctx);
+    }
+
+    @Override
+    public Integer visitParams(FantasticParser.ParamsContext ctx) {
+        return super.visitParams(ctx);
+    }
+
+    @Override
+    public Integer visitArgs(FantasticParser.ArgsContext ctx) {
+        return super.visitArgs(ctx);
+    }
+
+    @Override
+    public Integer visitBlock(FantasticParser.BlockContext ctx) {
+        return super.visitBlock(ctx);
     }
 }
